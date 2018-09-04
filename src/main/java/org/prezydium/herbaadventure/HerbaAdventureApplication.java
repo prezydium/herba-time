49bb59828a3763be89f2440c0dddd4f64b20f5d0
krzysztof.syska <prezydium834@gmail.com>
2018-09-04 20:10:34 +0200

    Initial commit

---
+++ src/main/java/org/prezydium/herbaadventure/HerbaAdventureApplication.java
@@ -0,0 +1,12 @@
+package org.prezydium.herbaadventure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerbaAdventureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerbaAdventureApplication.class, args);
+	}
+}
